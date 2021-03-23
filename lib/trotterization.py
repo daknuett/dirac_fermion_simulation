50ff053ae0acaed9631860261b1377dd051ee99d
Daniel Knüttel <daniel.knuettel@daknuett.eu>
2021-03-23 14:27:04 +0100

    fixed wrong usage of QFT

--- lib/trotterization.py
+++ lib/trotterization.py
@@ -17,7 +17,7 @@ def Ttrot(qbits_phi, qbits_px, qbits_py, dt, c, momentum_omegas):
 def TtrotLandau(qbits_phi, qbits_px, qbits_py, dt, c, B, momentum_omegas):
     circuit_list = []
     for qb_phi in qbits_phi:
-        circuit_list.append(QFT(qbits_px))
+        circuit_list.append(QFT(qbits_px).get_dagger())
         for j, qb_px in enumerate(qbits_px):
             circuit_list.append(CRY(qb_phi, qb_px, -dt * c * B * momentum_omegas[j]))
         circuit_list.append(QFT(qbits_px).get_dagger())
@@ -25,18 +25,18 @@ def TtrotLandau(qbits_phi, qbits_px, qbits_py, dt, c, B, momentum_omegas):
         circuit_list.append(QFT(qbits_py))
         for j, qb_py in enumerate(qbits_py):
             circuit_list.append(CRX(qb_phi, qb_py, dt * c * B * momentum_omegas[j]))
-        circuit_list.append(QFT(qbits_py).get_dagger())
+        circuit_list.append(QFT(qbits_py))
 
     return list_to_circuit(circuit_list)
 
 
 def Ttrot_potential(qbits_px, ancillas, dt, V0):
-    return (QFT(qbits_px)
+    return (QFT(qbits_px).get_dagger()
             | list_to_circuit([X(i) for i in qbits_px[-5:]])
             | C5R(qbits_px[0], qbits_px[-5:], ancillas, -dt * V0 / 2)
             | C5X(qbits_px[0], qbits_px[-5:], ancillas)
             | C5R(qbits_px[0], qbits_px[-5:], ancillas, -dt * V0 / 2)
             | C5X(qbits_px[0], qbits_px[-5:], ancillas)
             | list_to_circuit([X(i) for i in qbits_px[-5:]])
-            | QFT(qbits_px).get_dagger()
+            | QFT(qbits_px)
            )
